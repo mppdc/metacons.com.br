33bb4e0fba41ed262a6880f2fe92f3b5afce4a63
Marcos Carvalho <marcos@mcexpert.com.br>
2015-04-04 17:37:16 -0300

    operacao 2

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+var num1 = process.argv[2];
+var operador = process.argv[3];
+var num2 = process.argv[4];
+
+console.log (num1, operador, num2);
+
+if (operator =='+')
+{
+	console.log (num1 + num2);
+}
