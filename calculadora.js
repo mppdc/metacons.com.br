10ab9f94340e276dbd8294e2e68c32c79e01c8d0
Marcos Carvalho <marcos@mcexpert.com.br>
2015-04-04 17:42:26 -0300

    finaliza

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,6 @@
-var num1 = process.argv[2];
-var operador = process.argv[3];
-var num2 = process.argv[4];
+var num1 = process.argv[2]
+var operador = process.argv[3]
+var num2 = process.argv[4]
 
 console.log (num1, operador, num2);
 
